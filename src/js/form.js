00959737983c1a49226c0cd4501398f6043418cc
Anna <iaanya27@gmail.com>
2024-06-03 16:11:13 +0300

    added form js and styles

---
+++ src/js/form.js
@@ -0,0 +1,26 @@
+const formEl = document.querySelector('.login-form');
+let inputArr = document.querySelectorAll('input');
+inputArr = Array.from(inputArr);
+formEl.addEventListener('submit', myForm);
+function myForm(event) {
+  event.preventDefault();
+  let isValid = false;
+  // перевірка на ісвалід
+  inputArr.forEach(input => {
+    if (input.value) {
+      isValid = true;
+    } else {
+      isValid = false;
+      formEl.reset();
+    }
+  });
+  if (!isValid) {
+    alert('All form fields must be filled in');
+  } else {
+    let valueObj = { email: '', password: '' };
+    valueObj.email = inputArr[0].value;
+    valueObj.password = inputArr[1].value;
+    formEl.reset();
+    return console.log(valueObj);
+  }
+}
